dbe196d776b52c8187fb6baebdff18f8adc499f5
mat88-189 <mlucesoli@hotmail.com>
2024-04-23 13:37:03 +0200

    upd

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {'name': 'John Doe', 'age': 30, 'city': 'New York'}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
